033521d01e5ba290400ef7dc737376983d863552
minhyeoklee <rhodochrosited@gmail.com>
2019-03-15 10:13:19 +0900

    QuestionMaker

--- engine/data/question.py
+++ engine/data/question.py
@@ -12,13 +12,30 @@ class Question(object):
 class QuestionMaker(object):
     def __init__(self):
 
+        self.DEFAULT_CONFIG = config.DEFAULT_CONFIG
+
         self.preprocessor = PreProcessor()
 
         self.bert_model = Model()
         self.bert_model.build_model()
 
     def create_question(self, text, category, answer=None):
-        pass
+        '''
+
+        :param text: 질문
+        :param category: '셔틀', '학식', '잡담', '학사행정', '검색' ...
+        :param answer: 정답/ None
+        :return: Question object
+        '''
+        categories = self.DEFAULT_CONFIG['categories']
+
+        if category not in categories:
+            raise Exception('category must be ', categories)
+
+        input_feature = self.preprocessor.create_InputFeature(text)
+        feature_vector = self.bert_model.extract_feature_vector(input_feature)
+
+        return Question(text, category, answer, feature_vector)
 
 if __name__ == '__main__':
     qm = QuestionMaker()
