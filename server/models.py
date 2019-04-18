4ec00cd577488d8292548f94d1ef06168d1e5b99
minhyeoklee <rhodochrosited@gmail.com>
2019-04-19 00:03:37 +0900

    [+기능] get_questions_by_keywords

--- server/models.py
+++ server/models.py
@@ -1,9 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-
-class Member(models.Model):
-    name = models.CharField(max_length=200)
-    mail = models.CharField(max_length=200)
-    age = models.IntegerField(default=0)
