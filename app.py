41e94b9a58a1c45b613e1cbacb180833dfb97a5c
minhyeoklee <rhodochrosited@gmail.com>
2019-05-06 01:36:47 +0900

    [추가] remove 조사 at final output text

--- app.py
+++ app.py
@@ -2,4 +2,4 @@ from api.common.settings import *
 from api.resources.chatbot import CategorizeChat, Manager
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=6006, debug=True)
