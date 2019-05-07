e81ae28ed26a37bb96a0fc2b93f88d735965891d
minhyeoklee <rhodochrosited@gmail.com>
2019-05-08 08:44:39 +0900

    :wrench: Change configuration

--- api/common/settings.py
+++ api/common/settings.py
@@ -1,14 +1,12 @@
 from flask import Flask
 from flask_pymongo import PyMongo
-# from flask_restful import Api
 from flask_restplus import Api
 from flask_cors import CORS
-from werkzeug.contrib.fixers import ProxyFix
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/chatbot"
 CORS(app)
-api = Api(app, version=0.1, title='Hanyang-Chatbot')
+api = Api(app, version=0.2, title='Hanyang-Chatbot', description='수장이형은 우리가 이렇게 고생하는 걸 알까?')
 v1 = api.namespace('v1', description='version_1')
 db = PyMongo(app)
 
--- config.py
+++ config.py
@@ -5,17 +5,33 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 PREPROCESS = {
     'vocab_file': os.path.join(BASE_DIR, '../squad_train_model/vocab-9171.txt'),
     'use_morphs': True,
-    'max_seq_length': 25,
-    'max_query_length': 25,
+    'max_seq_length': 384,
+    'max_query_length': 64,
+    'keywords_tags': ['NNG', 'NNP', 'NNB', 'NNBC'],
+    'clean_orig_tags': ['JK', 'JX', 'JC']
 }
 
 BERT = {
     'model_path': os.path.join(BASE_DIR, '../squad_train_model/model.ckpt-11000'),
     'bert_json': os.path.join(BASE_DIR, '../squad_train_model/bert_config.json'),
     'feature_layers': -2,
-    'max_seq_length': 25,
-    'max_query_length': 25,
+    'max_seq_length': 384,
+    'max_query_length': 64,
 }
+# BERT = {
+#     'model_path': os.path.join(BASE_DIR, '../data/model_1/model.ckpt-100000'),
+#     'bert_json': os.path.join(BASE_DIR, './ckpt/bert_config.json'),
+#     'feature_layers': -2,
+#     'max_seq_length': 25,
+#     'max_query_length': 25,
+# }
+# PREPROCESS = {
+#     'vocab_file': os.path.join(BASE_DIR, './ckpt/vocab_mecab+khaiii_noBPE_5888'),
+#     'use_morphs': True,
+#     'max_seq_length': 25,
+#     'max_query_length': 25,
+#     'keywords_tag': ['NNG', 'NNP', 'NNB', 'NNBC'],
+# }
 
 HANDLER = {
     'distance': 'manhattan'  # euclidean, manhattan
@@ -25,16 +41,20 @@ QUESTION = {
     'categories': ['shuttle_bus', 'food', 'talk', 'search', 'book']
 }
 
+SEARCH = {
+    'sublinear_tf': True,
+}
+
 MONGODB = {
     'local_ip': 'localhost',
     'port': 27017,
     'db_name': 'chatbot',
     'col_questions': 'questions',
     'col_queries': 'queries',
+    'col_contexts': 'contexts',
     'username': "mhlee",
     'password': "mhlee"
 }
-
 
 if __name__ == '__main__':
     print(BASE_DIR)
