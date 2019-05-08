429e124373e3cb4b095a9209902e378f99d698f4
minhyeoklee <rhodochrosited@gmail.com>
2019-05-09 01:40:50 +0900

    :hammer: Reformated codes

--- engine/db/queries/query.py
+++ engine/db/queries/query.py
@@ -6,27 +6,21 @@ from engine.db.questions.index import find_by_text
 
 
 class Query(object):  # TODO 어떤 정보가 필요 할 지 계속 고민 해보기
-    def __init__(self, chat, feature_vector, keywords, matched_question, manhattan_similarity, jaccard_similarity):
-        '''
-
-        :param chat: str
-        :param feature_vector: array
-        :param keywords: list
-        :param matched_question: question object
-        :param manhattan_similarity: None
-        '''
+    def __init__(self, chat, feature_vector, keywords,
+                 matched_question, manhattan_similarity,
+                 jaccard_similarity, category=None):
         self.chat = chat
         self.feature_vector = feature_vector
         self.keywords = keywords
         self.matched_question = matched_question  # 어떤 질문과 매칭 되었었는지.
         self.manhattan_similarity = manhattan_similarity  # 거리는 어떠 하였는 지
         self.jaccard_similarity = jaccard_similarity
+        self.category = category
 
     def __str__(self):
         return 'Query chat:%5s, keywords%10s, question:%5s, feature_distance:%5.3f, jaccard_distance:%5.3f' \
-               % (
-                   self.chat, self.keywords, self.matched_question.text, self.manhattan_similarity,
-                   self.jaccard_distance)
+               % (self.chat, self.keywords, self.matched_question.text, self.manhattan_similarity,
+                  self.jaccard_similarity)
 
 
 def convert_to_query(document):
@@ -35,3 +29,9 @@ def convert_to_query(document):
     query = Query(document['chat'], feature_vector, document['keywords'],
                   matched_question, document['manhattan_similarity'], document['jaccard_similarity'])
     return query
+
+
+def convert_to_document(query):
+    query.feature_vector = pickle.dumps(query.feature_vector)
+    query.matched_question = query.matched_question.text
+    return query.__dict__
