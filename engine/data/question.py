7848ae662aa54eb22193f67866768345f8a0bce5
minhyeoklee <rhodochrosited@gmail.com>
2019-05-11 17:29:34 +0900

    :new: 카테고리 검사

--- engine/data/question.py
+++ engine/data/question.py
@@ -12,23 +12,16 @@ class QuestionMaker(object):
         self.preprocessor = PreProcessor()
 
     def create_question(self, text, category=None, answer=None):
-        '''
-
-        :param text: 질문
-        :param category: '셔틀', '학식', '잡담', '학사행정', '검색' ...
-        :param answer: 정답/ None
-        :return: Question object
-        '''
-        categories = self.CONFIG['categories']
-
-        # if category not in categories: # TODO 기능이 구체화 되면 다시 사용
-        #     raise Exception('category must be ', categories)
+        if category == 'Circle' or category == 'noanswer':
+            category = 'prepared'
+        if category not in self.CONFIG['categories']:
+            raise Exception('category must be ', self.CONFIG['categories'])
 
         keywords = self.preprocessor.get_keywords(text)
         morphs = self.preprocessor.get_morphs(text)
-        feature_vector = self.model_wrapper.similarity(text)
+        vector = self.model_wrapper.similarity(text)
 
-        return Question(text, category, answer, feature_vector, keywords, morphs)
+        return Question(text, category, answer, vector, morphs, keywords=keywords)
 
 
 if __name__ == '__main__':
