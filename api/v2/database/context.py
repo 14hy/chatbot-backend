eb043c6f7aba97566859285121e0ac86b4d50277
minhyeoklee <rhodochrosited@gmail.com>
2019-05-31 12:40:09 +0900

    JSON 직렬화되게 스트링으로변경

--- api/v2/database/context.py
+++ api/v2/database/context.py
@@ -20,7 +20,7 @@ class Context(Resource):
         parser.add_argument('text', type=str, required=True, help='등록 할 문단')
         args = parser.parse_args(strict=True)
 
-        return {'status': _context.create_insert(text=args['text'], subject=args['subject'])}
+        return {'status': str(_context.create_insert(text=args['text'], subject=args['subject']))}
 
 
 @api.route('/<string:_id>')
@@ -30,7 +30,7 @@ class Context(Resource):
         return id_to_str(_context.collection.find_one({'_id': _id}))
 
     def delete(self, _id):
-        return {'status': _context.collection.delete_one({'_id': _id})}
+        return {'status': str(_context.collection.delete_one({'_id': _id}))}
 
     @api.doc('문단 수정', params={'subject': '문단의 주제, 중복X', 'text': '문단'})
     def patch(self, _id):
@@ -49,7 +49,7 @@ class Context(Resource):
         if context:
             target['context'] = context
 
-        return {'status': _context.collection.update_one({'_id': _id}, update=target)}
+        return {'status': str(_context.collection.update_one({'_id': _id}, update=target))}
 
 
 @api.route('/<string:subject>')
@@ -60,7 +60,7 @@ class Context(Resource):
         return _context.collection.find_one({'subject': subject})
 
     def delete(self, subject):
-        return {'status': _context.collection.delete_one({'subject': subject})}
+        return {'status': str(_context.collection.delete_one({'subject': subject}))}
 
     @api.doc('문단 수정', params={'subject': '문단의 주제, 중복X', 'text': '문단'})
     def patch(self, subject):
@@ -79,7 +79,7 @@ class Context(Resource):
         if context:
             target['context'] = context
 
-        return {'status': _context.collection.update_one({'subject': subject}, update=target)}
+        return {'status': str(_context.collection.update_one({'subject': subject}, update=target))}
 
 
 @api.route('/<string:text>')
